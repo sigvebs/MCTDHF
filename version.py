a15ff17c693ac5a59754b6e5948f5f36798371ef
Sigve <sigvebs@gmail.com>
2013-03-12 10:49:01 +0100

    Temp. turining off the version script.

--- version.py
+++ version.py
@@ -4,7 +4,7 @@ Created on Fri Mar  1 09:13:07 2013
 
 @author: sigve
 """
-
+"""
 import sys
 import os
 from git import *
@@ -13,7 +13,7 @@ print sys.argv[1]
 print sys.argv[2]
 
 os.chdir(sys.argv[1])
-
+"""
 #repo = Repo()
 #text_file = open("%s/version.txt" %  sys.argv[2], "w")
 #text_file.write(repo.head.commit.hexsha)
